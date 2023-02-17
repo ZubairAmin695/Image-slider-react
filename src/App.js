578a1eef37187d22dbce37299ffa99860cb502da
ZubairAmin <zubairameen.official@gmail.com>
2023-02-17 12:26:39 +0500

    image slider

---
+++ src/App.js
@@ -0,0 +1,14 @@
+ 
+import './App.css';
+import Slider from './component/Slider';
+
+function App() {
+  return (
+   
+   <>
+   <Slider/>
+   </>
+  );
+}
+
+export default App;
---
+++ src/component/Slider.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./slider.css";
+
+const Slider = () => {
+  return (
+    <div className="s-container">
+      <div className="slider-container" id="pause">
+        
+      <div className="slide-image">
+            <img src="/assets/5.jpg" alt="image"/>
+        </div> <div className="slide-image">
+            <img src="/assets/5.jpg" alt="image"/>
+        </div> <div className="slide-image">
+            <img src="/assets/5.jpg" alt="image"/>
+        </div> <div className="slide-image">
+            <img src="/assets/5.jpg" alt="image"/>
+        </div> <div className="slide-image">
+            <img src="/assets/5.jpg" alt="image"/>
+        </div> <div className="slide-image">
+            <img src="/assets/5.jpg" alt="image"/>
+        </div> <div className="slide-image">
+            <img src="/assets/5.jpg" alt="image"/>
+        </div> <div className="slide-image">
+            <img src="/assets/5.jpg" alt="image"/>
+        </div> <div className="slide-image">
+            <img src="/assets/5.jpg" alt="image"/>
+        </div> <div className="slide-image">
+            <img src="/assets/5.jpg" alt="image"/>
+        </div> <div className="slide-image">
+            <img src="/assets/5.jpg" alt="image"/>
+        </div> <div className="slide-image">
+            <img src="/assets/5.jpg" alt="image"/>
+        </div> <div className="slide-image">
+            <img src="/assets/5.jpg" alt="image"/>
+        </div> <div className="slide-image">
+            <img src="/assets/5.jpg" alt="image"/>
+        </div> <div className="slide-image">
+            <img src="/assets/5.jpg" alt="image"/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
